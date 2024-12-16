b731a3b1637a7e83318c1514321c7bc30cbf13d2
Дастан Маккамбаев <makkambaevdastan@gmail.com>
2024-12-17 00:28:53 +0600

    .

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
     // Scanner scanner = new Scanner(System.in);
     // int a = scanner.nextInt();
     // int b = scanner.nextInt();
